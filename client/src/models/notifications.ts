2bec10670ed7ab8d93a0895457f1a497ffd691fc
Jaheen Reza <rezajaheen@gmail.com>
2025-03-24 14:50:45 -0400

    rest extension

---
+++ client/src/models/notifications.ts
@@ -0,0 +1,25 @@
+/*  B"H
+ */
+
+import { ref } from 'vue'
+
+export interface Notification {
+  message: string
+  type: 'success' | 'error' | 'warning' | 'info'
+}
+
+const notifications = ref<Notification[]>([])
+
+export const refNotifications = () => notifications
+
+export function addNotification(notification: Notification) {
+  notifications.value.push(notification)
+}
+
+export function removeNotification(index: number) {
+  notifications.value.splice(index, 1)
+}
+// export function removeNotification(index: number) {
+//     notifications.value = notifications.value.filter((_, i) => i !== index)
+// }
+
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from '@/components/NavBar.vue'
+import NotificationList from '@/components/NotificationList.vue';
+import SlidingDrawer from '@/components/SlidingDrawer.vue';
+import ShoppingCart from '@/components/ShoppingCart.vue';
+import { ref } from 'vue';
 
-var bestClass = 'Web Programming'
+const isShoppingCartOpen = ref(false)
+
 </script>
 
 <template>
-  <main>
-    <h1 class="title">Welcome to {{ bestClass }}</h1>
-  </main>
+  <header>
+    <NavBar v-model:is-shopping-cart-open="isShoppingCartOpen" />
+  </header>
+
+  <div class="container body-container">
+    <NotificationList />
+    <RouterView />
+  </div>
+
+  <SlidingDrawer :isOpen="isShoppingCartOpen">
+    <ShoppingCart />
+  </SlidingDrawer>
 </template>
+
+<style>
+body {
+  background-color: aliceblue;
+}
+
+.body-container {
+  background-color: white;
+  height: 100vh;
+  padding: 1rem;
+  padding-top: 4em;
+}
+</style>
